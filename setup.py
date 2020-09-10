358adfea790f843e335e8e6f2422638edc1158b3
永格天 <we684123@gmail.com>
2020-09-11 01:42:51 +0800

    pypi V1.0.0 終於! Orz
    經過一波20折 終於!

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding='utf-8') as fh:
-    long_description = fh.read()
+    long_description = fh.read().rstrip()
 
 setup(
     name="get_filename",
-    version="0.0.1",
+    version="1.0.0",
     keywords=("filename", "extension"),
     description="from a filename(str) extension or name",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     license="MIT",
     url="https://github.com/we684123/get_filename",
     author="we684123",
